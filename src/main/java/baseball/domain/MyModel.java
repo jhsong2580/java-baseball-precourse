032fed336e75b91f582a7f5aa4f1a0b7ea3458fe
sjh <thdwmdgns@naver.com>
2022-04-12 17:51:22 +0900

    feat(MyModel.java) : init class
    BASEBALL_MAX : Baseball 정답 입력/생성 최대값
    BASEBALL_MIN : Baseball 정답 입력/생성 최소값
    ANSWER : 현재 진행중인 게임의 정답
    isEND : 게임이 완전히 종료되었는지 여부

---
+++ src/main/java/baseball/domain/MyModel.java
@@ -0,0 +1,10 @@
+package baseball.domain;
+
+public class MyModel {
+    private final int BASEBALL_MAX = 999;
+    private final int BASEBALL_MIN = 111;
+    private String answer="";
+    private boolean isEnd=false;
+
+
+}
