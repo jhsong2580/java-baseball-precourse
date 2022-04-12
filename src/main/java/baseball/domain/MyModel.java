e566febcc0348de2eea488b052515b9c0427be1a
sjh <thdwmdgns@naver.com>
2022-04-12 18:07:51 +0900

    feat(MyModel.java) : create initGame method
    게임 상태 초기화 함수 생성
    answer 값 주입(111~999내 0이 들어가지 않은 숫자)
    isEnd 값 주입(false)

--- src/main/java/baseball/domain/MyModel.java
+++ src/main/java/baseball/domain/MyModel.java
@@ -1,10 +1,31 @@
 package baseball.domain;
 
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+
 public class MyModel {
     private final int BASEBALL_MAX = 999;
     private final int BASEBALL_MIN = 111;
     private String answer="";
     private boolean isEnd=false;
 
+
+    public void initGame(){
+        generateRandomNumber();
+        isEnd = false;
+    }
+
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public boolean isEnd() {
+        return isEnd;
+    }
+
+    private void generateRandomNumber(){
+        for(int i=0;i<3;i++)
+            answer+=String.valueOf(pickNumberInRange(1,9));
+    }
 
 }
