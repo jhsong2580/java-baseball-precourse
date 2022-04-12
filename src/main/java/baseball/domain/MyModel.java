20b98eaaff1488d1ca72bc9dd4509423046cf50a
sjh <thdwmdgns@naver.com>
2022-04-12 19:25:11 +0900

    feat(MyModel.java) : create validateInput,endGame method
    ㅇvalidateInput : 게임 상태 별 입력값 검증 로직
     - parseStringToInteger : 입력받은 String을 Int로 parse. Parse 불가시 parseStringToInteger 발생
     - isEnd를 분기로 게임중일때의 검증로직과 게임중이 아닐때의 검증로직 처리
       - validateInputNotInGame : 게임이 종료되었을때 실행되는 검증로직
         - Input이 1,2일때만 정상처리 그 외는 IllegalArgumentException 발생
       - validateInputInGame    : 게임이 진행중일때 실행되는 검증로직
         - Input이 111~999값 이내이고 0이 존재하지 않을때 정상처리. 그 외는 IllegalArgumentException 발생
    
    ㅇendGame : 정답을 맞춘 후 게임 종료
     - isEnd를 true로 변경

--- src/main/java/baseball/domain/MyModel.java
+++ src/main/java/baseball/domain/MyModel.java
@@ -14,6 +14,20 @@ public class MyModel {
         isEnd = false;
     }
 
+    public void endGame(){
+        isEnd = true;
+    }
+
+    public boolean validateInput(String input){
+
+        int inputCast = parseStringToInteger(input);
+
+        boolean validateResult = validateInputNotInGame(inputCast) && validateInputInGame(inputCast);
+        if(!validateResult)
+            throw new IllegalArgumentException();
+        return true;
+
+    }
 
     public String getAnswer() {
         return answer;
@@ -21,6 +35,39 @@ public class MyModel {
 
     public boolean isEnd() {
         return isEnd;
+    }
+    private boolean validateInputNotInGame(int input){
+        if(!isEnd)
+            return true;
+        return checkInputOneOrTwo(input);
+    }
+    private boolean checkInputOneOrTwo(int input){
+        return input == 1 || input == 2;
+    }
+    private boolean validateInputInGame(int input){
+        if(isEnd)
+            return true;
+        return checkInputWithRange(input) && checkInputWithoutZero(input,2);
+    }
+    private int parseStringToInteger(String input){
+        try{
+            return Integer.parseInt(input);
+        }catch(NumberFormatException e){
+            throw new IllegalArgumentException();
+        }
+
+    }
+    private boolean checkInputWithRange(int input){
+        return input >= BASEBALL_MIN && input <= BASEBALL_MAX;
+    }
+    private boolean checkInputWithoutZero(int input,int index){
+        if(index == -1)
+            return true;
+        int divResult = (int)(input / Math.pow(10, index));
+        if(divResult == 0)
+            throw new IllegalArgumentException();
+        input -=divResult*Math.pow(10,index);
+        return checkInputWithoutZero(input,--index);
     }
 
     private void generateRandomNumber(){
