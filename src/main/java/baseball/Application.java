9181548c216c0ecb7bdb242f33ba0611ce321873
sjh <thdwmdgns@naver.com>
2022-04-15 12:06:49 +0900

    !BREAKING CHANGE(Application.java): MyModel 생성자 추가에 따른 인자값 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,7 +8,7 @@ import baseball.view.MyView;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        MyModel model = new MyModel();
+        MyModel model = new MyModel(3);
         MyView view = new MyView();
         MyController controller = new MyController(model, view);
  
