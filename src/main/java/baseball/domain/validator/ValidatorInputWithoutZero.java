9ebe107e7cdf270825acbe8d96a4ca1cdb3a6960
sjh <thdwmdgns@naver.com>
2022-04-16 13:11:57 +0900

    feat(ValidatorInputWithoutZero.java): 게임 중일때 0 입력 여부 확인 구현체

---
+++ src/main/java/baseball/domain/validator/ValidatorInputWithoutZero.java
@@ -0,0 +1,26 @@
+package baseball.domain.validator;
+
+public class ValidatorInputWithoutZero implements MyValidator {
+    private final int BASEBALL_ANSWER_SIZE;
+
+    public ValidatorInputWithoutZero(int BASEBALL_ANSWER_SIZE) {
+        this.BASEBALL_ANSWER_SIZE = BASEBALL_ANSWER_SIZE;
+    }
+
+    @Override
+    public boolean canValidate(boolean isEnd) {//게임이 진행중일때 검증 가능
+        return !isEnd;
+    }
+
+    @Override
+    public boolean validate(int input) {
+        boolean isWithoutZero = true;
+        String inputCastString = String.valueOf(input);
+        for(int i=0;i<BASEBALL_ANSWER_SIZE;i++){
+            isWithoutZero = isWithoutZero && (inputCastString.charAt(i)!= '0');
+        }
+        return isWithoutZero;
+    }
+
+
+}
