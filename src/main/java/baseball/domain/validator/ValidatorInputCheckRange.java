7def39e82449eb5953188ea94d7348a13779a93b
sjh <thdwmdgns@naver.com>
2022-04-16 13:11:19 +0900

    feat(ValidatorInputCheckRange.java): 게임이 중일때 입력 범위 검증 구현체

---
+++ src/main/java/baseball/domain/validator/ValidatorInputCheckRange.java
@@ -0,0 +1,21 @@
+package baseball.domain.validator;
+
+public class ValidatorInputCheckRange implements MyValidator {
+    private final int BASEBALL_MAX ;
+    private final int BASEBALL_MIN ;
+
+    public ValidatorInputCheckRange(int BASEBALL_ANSWER_SIZE) {
+        this.BASEBALL_MAX = (int)Math.pow(10,BASEBALL_ANSWER_SIZE) -1;
+        this.BASEBALL_MIN = (int)Math.pow(10,BASEBALL_ANSWER_SIZE-1);
+    }
+
+    @Override
+    public boolean canValidate(boolean isEnd) { //게임이 진행중일때 검증 가능
+        return !isEnd;
+    }
+
+    @Override
+    public boolean validate(int input) {
+        return input >= BASEBALL_MIN && input <= BASEBALL_MAX;
+    }
+}
